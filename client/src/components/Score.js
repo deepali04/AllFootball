caabce911b2cffbe44e5e5e3920ccf9ea71fe376
deepali04 <nagwade.deepali@gmail.com>
2023-06-01 15:34:21 -0400

    Updated UI in score.js

--- client/src/components/Score.js
+++ client/src/components/Score.js
@@ -81,9 +81,43 @@ if(data ){
     
   
     return(
-        <div className="row justify-content-center" id='home'>
-            <button className='btn btn-success' onClick={(event) => handle_score(event)}>GET SCORE</button>
+        <div className="row justify-content-center align-items-center" id='home' style={{ height: '100vh' }}>
+
+        <div className="row justify-content-center align-items-center">
+
+            <div className='col-md-3 mr-5 ml-2 p-0'>
+                <div className="wsk-cp-matches " >                              
+                    <h4>Your Games and Score Information</h4>                               
+                    <hr style={{ background: "#D3D3D3",height: "2px", border: "none", opacity:0.1}}/>      
+                    <div className='row'>
+                        <p className="tablehead">
+                            You can play a game on the day of match itself. Whenever you play a game and you predict correctly, you get 100 points for correct prediction and your score will be updated after each game.
+                            <br></br>
+                            Click on MY SCORE button to check your score!
+                        </p>
+                    </div>    
+                </div>
+            </div>
+
+
+            <div className='col-md-3 mr-5 ml-2 p-0'>
+                <div className="wsk-cp-matches " >                              
+                    <h4>Upgrade to Premium Account Member</h4>                               
+                    <hr style={{ background: "#D3D3D3",height: "2px", border: "none", opacity:0.1}}/>      
+                    <div className='row'>
+                        <p className="tablehead">
+                            You can 100 points per correct prediction and when you have total 200 points you'll be upgraded to premium member and you have access to our premium features where you can follow youe favorite team and players so that you can access their stats easily.
+                        </p>
+                    </div>    
+                </div>
+            </div>
         </div>
+
+
+            <div className="col-4">
+                <button className='btn btn-outline-info' onClick={(event) => handle_score(event)}>GET SCORE</button>
+            </div>
+</div>
 
     )
     
